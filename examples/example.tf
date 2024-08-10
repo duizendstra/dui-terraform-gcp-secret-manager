c21f53c10ced5293a04427687b03387e04ec1f12
development <development@duizendstra.com>
2024-08-10 10:30:29 +0000

    fix

--- examples/example.tf
+++ examples/example.tf
@@ -1,24 +1,51 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.40.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 5.40.0"
+    }
+  }
+}
+
 module "secret_manager" {
   source = "./.."
 
+  # project = {
+  #   project_id = "your-project-id"
+  #   project_services = {
+  #     "secretmanager.googleapis.com" = {}
+  #   }
+  # }
+
   project = {
-    project_id = "your-project-id"
-    project_services = {
-      "secretmanager.googleapis.com" = {}
+    "project_id" = "dui-module-test-de27"
+    "project_services" = {
+      "secretmanager.googleapis.com" = {
+
+      }
     }
   }
 
   secrets = [{
     secret_id   = "your-secret-id"
     secret_data = "your-secret-data"
-    members = [
+    iam_members = [
       {
         id     = "your-member-id"
-        member = "user:user@example.com"
+        member = "user:jasper@duizendstra.com"
         roles = [
           "roles/owner"
         ]
       }
     ]
   }]
+}
+
+output "secret_manager" {
+  description = "The IAM members and their roles"
+  value       = module.secret_manager.secret_manager
 }
